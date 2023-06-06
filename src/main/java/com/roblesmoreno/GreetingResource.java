fa071019faae783dc1ed5c58547de2647c000965
Rodolfo Robles <rm.rodolfo@outlook.com>
2023-06-06 18:14:46 -0400

    Chapter 01
    Completed:
    - Json logger
    - Endpoints hello and world
    - dependeciy for logging.log4j

--- src/main/java/com/roblesmoreno/GreetingResource.java
+++ src/main/java/com/roblesmoreno/GreetingResource.java
@@ -4,13 +4,28 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.jboss.logging.Logger;
 
 @Path("/hello")
 public class GreetingResource {
 
+    private static final Logger log = Logger.getLogger(GreetingResource.class);
+    @ConfigProperty(name="publisher.name")
+    String publisherName;
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
+        log.info(String.format("Property %s",publisherName) );
         return "Hello from RESTEasy Reactive";
+    }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/world")
+    public Response world(){
+        return Response.status(Response.Status.OK).entity("Hello World").build();
     }
 }
